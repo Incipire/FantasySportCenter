dea68b7588727f9df69721ffaa0d82a27a09afe9
Michael Brown <mbrown@incipire.com>
2014-09-07 12:15:37 -0500

    List of current games (hardcoded)

--- src/fscdotcom/fsc/urls.py
+++ src/fscdotcom/fsc/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^games/', include('games.urls'))
 )
--- src/fscdotcom/games/views.py
+++ src/fscdotcom/games/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
 
-# Create your views here.
+from models import Game
+
+def index(request):
+	games=Game.objects.filter(season_year=2014).filter(week=1).filter(season_type='Regular')
+	context = RequestContext(request, {
+		})
+	return render(request, 'games/index.html', 
+			{'games_list':games})
+
+
+def detail(request, gsid):
+	try:
+		game = Game.objects.get(pk=gsid)
+	except Game.DoesNotExist:
+		raise Http404
+	return render(request, 'games/detail.html', {'game':game})
