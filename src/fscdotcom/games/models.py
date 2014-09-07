ae5ebe141e971aec74ec51568b73d21523e955d8
Michael Brown <mbrown@incipire.com>
2014-09-07 03:46:30 -0500

    manual model

--- src/fscdotcom/games/models.py
+++ src/fscdotcom/games/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+	gsis_id=models.CharField(max_length=10,primary_key=True)
+	start_time=models.DateTimeField('Game Start Time')
+	finished=models.BooleanField()
+#Home team data
+	home_team=models.CharField(max_length=30)
+	home_score=models.SmallIntegerField()
+	home_score_Q1=models.SmallIntegerField()
+	home_score_Q2=models.SmallIntegerField()
+	home_score_Q3=models.SmallIntegerField()
+	home_score_Q4=models.SmallIntegerField()
+	home_score_Q5=models.SmallIntegerField()
+#Away team data
+	away_team=models.CharField(max_length=30)
+	away_score=models.SmallIntegerField()
+	away_score_Q1=models.SmallIntegerField()
+	away_score_Q2=models.SmallIntegerField()
+	away_score_Q3=models.SmallIntegerField()
+	away_score_Q4=models.SmallIntegerField()
+	away_score_Q5=models.SmallIntegerField()
