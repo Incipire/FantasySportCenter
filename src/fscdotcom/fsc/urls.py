c5f23111079f6da58e3eccf9c3eec9425e6b674b
Michael Brown <mbrown@incipire.com>
2014-09-10 16:39:21 -0500

    Updated url mappings

--- src/fscdotcom/fsc/urls.py
+++ src/fscdotcom/fsc/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^games/', include('games.urls'))
+    url(r'^games/', include('games.urls')),
+    url(r'^$', include('games.urls')),
 )
