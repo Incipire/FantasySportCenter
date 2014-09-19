d1f80052b048862bb447351fd81981329cb87a4e
Michael Brown <mbrown@incipire.com>
2014-09-19 18:15:06 -0500

    Added basic player_detail view function and template updated anchor tags to use django url tag

--- src/fscdotcom/games/views.py
+++ src/fscdotcom/games/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 import nfldb
-from models import Game,Meta
 
 def index(request,year=0,week=0,season_type='Regular'):
     db = nfldb.connect()
@@ -14,10 +13,19 @@ def index(request,year=0,week=0,season_type='Regular'):
         return redirect('index',year, season_type.name, week)
     q =nfldb.Query(db).game(season_year=year, season_type=season_type, week=week).sort('start_time')
     games=q.as_games()
-    #games=Game.objects.filter(season_year=2014).filter(week=1).filter(season_type='Regular')
     return render(request, 'games/index.html', 
             {'games_list':games, 'current':current, 'year':year})
 
+
+def player_detail(request, player_id):
+    db = nfldb.connect()
+    q = nfldb.Query(db).player(player_id=player_id)
+    players = q.as_players()
+    if (len(players)!=1):
+        raise Http404
+    player = players[0]
+    return render(request, 'games/player_detail.html',
+            {'player':player})
 
 def detail(request, gsid):
     db = nfldb.connect()
